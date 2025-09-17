6e9126ffb326743f64e45c0fecfd14355c7e8f54
Luiz Colman <colmanluizz@gmail.com>
2025-09-17 16:37:18 -0300

    feat(throttler): configured global throttling

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
@@ -9,9 +11,21 @@ import { AuthModule } from "./auth/auth.module";
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot({
+      throttlers: [{
+        ttl: 1000, // 1 second
+        limit: 10, // 10 requests per second
+      }],
+    }),
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
