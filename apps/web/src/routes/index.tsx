7cdfc6000d67a73ca5df2ef52f73bc00b706ab38
Luiz Colman <colmanluizz@gmail.com>
2025-09-24 16:43:07 -0300

    feat(dashboard): added a sample dashboard page

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useAuth } from '../context/AuthContext'
 
+import { AppSidebar } from '@/components/app-sidebar'
+import { ChartAreaInteractive } from '@/components/chart-area-interactive'
+import { DataTable } from '@/components/data-table'
+import { SectionCards } from '@/components/section-cards'
+import { SiteHeader } from '@/components/site-header'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+
+import data from '../lib/data.json'
+
 export const Route = createFileRoute('/')({
   component: HomePage,
 })
@@ -20,5 +29,30 @@ function HomePage() {
     )
   }
 
-  return <div>teste</div>
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <DataTable data={data} />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
