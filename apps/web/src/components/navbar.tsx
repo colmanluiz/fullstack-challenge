4917283fcb1e1f0bd90b47972cce2ea2ac17a5a7
Luiz Colman <colmanluizz@gmail.com>
2025-09-24 16:42:42 -0300

    feat(navbar): add search input and update layout styles

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { useId } from 'react'
-import { LayoutDashboard, LucideListTodo } from 'lucide-react'
+import { LayoutDashboard, LucideListTodo, SearchIcon } from 'lucide-react'
 
 import Logo from '@/components/navbar-components/logo'
 import NotificationMenu from '@/components/navbar-components/notification-menu'
@@ -19,11 +19,12 @@ import {
 import ThemeToggle from './navbar-components/theme-toggle'
 import { Link } from '@tanstack/react-router'
 import { useAuth } from '@/context/AuthContext'
+import { Input } from './ui/input'
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
-  { href: '#', label: 'Dashboard', icon: LayoutDashboard, active: true },
-  { href: '#', label: 'Tasks', icon: LucideListTodo },
+  { href: '/', label: 'Dashboard', icon: LayoutDashboard, active: true },
+  { href: '/tasks', label: 'Tasks', icon: LucideListTodo },
 ]
 
 export default function Navbar() {
@@ -31,7 +32,7 @@ export default function Navbar() {
   const id = useId()
 
   return (
-    <header className="border-b px-4 md:px-6">
+    <header className="px-4 md:px-6 bg-[#fbfbfb] dark:bg-[#0f0f0f]">
       <div className="flex h-16 items-center justify-between gap-4">
         {/* Left side */}
         <div className="flex flex-1 items-center gap-2">
@@ -104,32 +105,22 @@ export default function Navbar() {
           </div>
         </div>
         {/* Middle area */}
-        <NavigationMenu className="max-md:hidden">
-          <NavigationMenuList className="gap-2">
-            {navigationLinks.map((link, index) => {
-              const Icon = link.icon
-              return (
-                <NavigationMenuItem key={index}>
-                  <NavigationMenuLink
-                    active={link.active}
-                    href={link.href}
-                    className="text-foreground hover:text-primary flex-row items-center gap-2 py-1.5 font-medium"
-                  >
-                    <Icon
-                      size={16}
-                      className="text-muted-foreground/80"
-                      aria-hidden="true"
-                    />
-                    <span>{link.label}</span>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              )
-            })}
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="flex flex-1 items-center justify-end gap-2">
+          <div className="relative">
+            <Input
+              id={id}
+              className="peer h-8 ps-8 pe-2"
+              placeholder="Search..."
+              type="search"
+            />
+            <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-2 peer-disabled:opacity-50">
+              <SearchIcon size={16} />
+            </div>
+          </div>
+        </div>
         {/* Right side */}
         {user && isAuthenticated ? (
-          <div className="flex flex-1 items-center justify-end gap-4">
+          <div className="flex items-center justify-end gap-4">
             {/* Notification */}
             <NotificationMenu />
             {/* User menu */}
@@ -137,7 +128,7 @@ export default function Navbar() {
             <ThemeToggle />
           </div>
         ) : (
-          <div className="flex flex-1 items-center justify-end gap-2">
+          <div className="flex items-center justify-end gap-2">
             <Button asChild variant="ghost" size="sm" className="text-sm">
               <Link to="/login">Login</Link>
             </Button>
