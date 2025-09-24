27400abf3cd8066b850771b6a32ff6a66ae4e583
Luiz Colman <colmanluizz@gmail.com>
2025-09-24 17:09:24 -0300

    feature(layout): added sidebar to root layout

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -5,6 +5,8 @@ import { TanstackDevtools } from '@tanstack/react-devtools'
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
+import { AppSidebar } from '@/components/app-sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import Navbar from '@/components/navbar'
 
 interface MyRouterContext {
@@ -14,10 +16,24 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <div className="min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
+      <SidebarProvider
+        style={
+          {
+            '--sidebar-width': 'calc(var(--spacing) * 72)',
+            '--header-height': 'calc(var(--spacing) * 12)',
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <Navbar />
+          <main className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col">
+              <Outlet />
+            </div>
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
       <TanstackDevtools
         config={{
           position: 'bottom-left',
